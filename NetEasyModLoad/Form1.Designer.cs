e55a0efd6ae7ebe4510265aae20ceb30d87d08d0
Long-Zixuan <778275146@qq.com>
2025-08-15 14:51:40 +0800

    First Commit   LZX-2025-08-15-001

---
+++ NetEasyModLoad/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace NetEasyModLoad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.choose_button = new System.Windows.Forms.Button();
+            this.button_load = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // choose_button
+            // 
+            this.choose_button.Location = new System.Drawing.Point(66, 31);
+            this.choose_button.Name = "choose_button";
+            this.choose_button.Size = new System.Drawing.Size(637, 53);
+            this.choose_button.TabIndex = 0;
+            this.choose_button.Text = "选择网易我的世界文件夹(MCLDownload)";
+            this.choose_button.UseVisualStyleBackColor = true;
+            this.choose_button.Click += new System.EventHandler(this.choose_button_Click);
+            // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(66, 126);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(637, 59);
+            this.button_load.TabIndex = 1;
+            this.button_load.Text = "加载";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 228);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_load);
+            this.Controls.Add(this.choose_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "网易我的世界第三方模组加载器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button choose_button;
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ NetEasyModLoad/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace NetEasyModLoad
+{
+    public partial class Form1 : Form
+    {
+        string gamePath = "";
+
+        string loaderExePath = "";
+        string configPath = "";
+
+        string[] mods;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            loaderExePath = System.Windows.Forms.Application.StartupPath;
+            configPath = loaderExePath + @"\config.ini";
+            if (!File.Exists(configPath))
+            {
+                FileStream fs = File.Create(loaderExePath + @"\config.ini");
+                fs.Close();
+            }
+            label3.Text = "当前游戏文件夹："+ File.ReadAllText(configPath);
+            gamePath = File.ReadAllText(configPath);
+            mods = Directory.GetFiles(loaderExePath+@"\mods", "*.jar");
+            //MessageBox.Show(mods[0]);
+            //MessageBox.Show(loaderExePath);
+        }
+
+        private void choose_button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            // folderBrowser.SelectedPath = webpath;
+            folderBrowser.Description = "请选择网易我的世界模组文件夹";
+            //folderBrowser.ShowNewFolderButton = true;
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                gamePath = folderBrowser.SelectedPath + @"\Game\.minecraft\mods";
+                label3.Text = "当前游戏文件夹："+gamePath;
+                File.WriteAllText(configPath, gamePath);
+            }
+                        
+        }
+
+        private void button_load_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(gamePath))
+            {
+                MessageBox.Show("文件夹不存在");
+                return;
+            }
+            label2.Text = "开始加载";
+            string filePath = gamePath + "/isLoaded.jar";
+            FileStream fs = File.Create(filePath);
+            fs.Close();
+
+            while (true)
+            {
+                if (!File.Exists(filePath))
+                {
+                    foreach (string mod in mods)
+                    {
+                        FileInfo file = new FileInfo(mod);
+                        string fileName = Path.GetFileName(mod);
+                        if (file.Exists)
+                        {
+                            //true 覆盖已存在的同名文件，false不覆盖
+                            file.CopyTo(gamePath+"/"+fileName, true);
+                        }
+                        else
+                        {
+                            MessageBox.Show("文件" + mod + "不存在");
+                        }
+                    }
+                    break;
+                }
+                Thread.Sleep(1000);
+            }
+            label2.Text = "完成加载";
+        }
+    }
+}
