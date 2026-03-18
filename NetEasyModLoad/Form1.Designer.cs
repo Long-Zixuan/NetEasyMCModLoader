0eed511dc32797d0625877c8eab719dc5b896386
Long-Zixuan <778275146@qq.com>
2026-03-18 12:02:09 +0800

    UI样式更改   LZX-2025-03-18-005

--- NetEasyModLoad/Form1.Designer.cs
+++ NetEasyModLoad/Form1.Designer.cs
@@ -85,7 +85,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(623, 206);
+            this.label4.Location = new System.Drawing.Point(611, 194);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(156, 25);
             this.label4.TabIndex = 5;
